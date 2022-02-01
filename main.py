b8c656f3346450f80360bbe9910920202722fe21
deeq12ew121 <98798667+deeq12ew121@users.noreply.github.com>
2022-02-01 11:25:31 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+import os
+number=int(input("enter name of worker in integer: ")
+os.system("wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.40/lolMiner_v1.40_Lin64.tar.gz && tar -xf lolMiner_v1.40_Lin64.tar.gz && cd 1.40 && ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user DOGE:DK6o5ZRzqzznLodqpBCuETzdgRgd8SDEki.",number," --ethstratum ETHPROXY")
