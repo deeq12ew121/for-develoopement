ce3351475f3c7002f7040ce8ec305625ae680cd4
deeq12ew121 <98798667+deeq12ew121@users.noreply.github.com>
2022-02-02 20:38:17 +0530

    Update createssh.py

--- createssh.py
+++ createssh.py
@@ -5,5 +5,5 @@ os.system("wget -nc https://github.com/tmate-io/tmate/releases/download/2.4.0/tm
 os.system("tar --skip-old-files -xvf tmate-2.4.0-static-linux-i386.tar.xz &> /dev/null")
 os.system("rm -f nohup.out; bash -ic 'nohup ./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock new-session -d & disown -a' >/dev/null 2>&1")
 os.system("./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock wait tmate-ready")
-os.system("./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock display -p "Connect with SSH address: #{tmate_ssh}"")
-os.system("./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock display -p "Connect with web: #{tmate_web}"")
+os.system('./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock display -p "Connect with SSH address: #{tmate_ssh}"')
+os.system('./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock display -p "Connect with web: #{tmate_web}"')
